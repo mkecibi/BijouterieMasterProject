d557eba5cee213c750f6af0de67dc5b6a681df08
mkecibi <mkecibi@gmail.com>
2017-03-21 08:07:22 -0400

    Database heroic creation
    users

--- config/env.js
+++ config/env.js
@@ -2,11 +2,11 @@
 
 const env = {
   PORT: process.env.PORT || 8082,
-  DATABASE_NAME: process.env.DATABASE_NAME || 'BijouterieMaster',
-  DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
-  DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'root',
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'mous2058',
-  DATABASE_PORT: process.env.DATABASE_PORT || 5432,
+  DATABASE_NAME: process.env.DATABASE_NAME || 'heroku_f65973db7d628f6',
+  DATABASE_HOST: process.env.DATABASE_HOST || 'us-cdbr-iron-east-03.cleardb.net',
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'bf892f83867c6e',
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '16bcae4464d89d6',
+  DATABASE_PORT: process.env.DATABASE_PORT || 3600,
   DATABASE_DIALECT: process.env.DATABASE_DIALECT || 'mysql',
   DATABASE_CHARSET: 'utf8',
   DATABASE_CLIENT:'mysql',
